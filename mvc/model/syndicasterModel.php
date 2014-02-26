83602edfc0939091759a495eaf90f409d9e34707
Desarrollo <Desarrollo@mbp-de-yair.gateway.2wire.net>
2014-02-26 11:18:21 -0600

    :(

--- mvc/model/syndicasterModel.php
+++ mvc/model/syndicasterModel.php
@@ -48,7 +48,7 @@ class syndicaster {
 	}
 
 	public function getPlays($id) {
-		$path='reports/views'; //aqui hice un cambio
+		$path='reports/views';
 		$token=$this->token();
 		$curlheader[0] = "Content-Type: application/json";
         $curlheader[1] = "Authorization: OAuth ".$token;
