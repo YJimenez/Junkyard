7c7b96f864d1bdd6f768780bcbc8677ab4ed87fa
etorrres <etorres@junkyad.mx>
2014-03-25 11:14:12 -0600

    .

--- middleware/mvc/controller/newPlaylistController.php
+++ middleware/mvc/controller/newPlaylistController.php
@@ -2,7 +2,13 @@
 
 if(!isset($_SESSION['ooyala']))
 header("Location: index.php?section=login");
-
+	
+	$permiso=new permisos();
+if(!$permiso->vPermiso("1", $_SESSION['ooyalaUser']['profile'])&&!$_SESSION['ooyalaUser']['admin']) {
+		echo "You don't have privileges to access this section";
+		$permiso->redirige("?section=home",3);
+		exit();
+	}
 
 	$api = new OoyalaApi("V5dzkxOmUFf0dFju2v9bPHqRdgjC.0Ut0Y", "O7PUVcRVGXQx5HtqMlt7MoS8wrBr_FByN-J11-s_");
 	$playlist=$api->get("/v2/playlists");
